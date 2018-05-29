bf4c731375e670dec25e9f08e888a9e90f8f4db7
Unknown <jb.gandara92@gmail.com>
2018-05-29 04:52:52 +0200

    Terminada la maquetación del HOME y los estilos

---
+++ buscador/search.php
@@ -0,0 +1,8 @@
+<?php include '../funciones.php';?>
+    <!-- CABECERA -->
+    <?php get_header_subfolder('Encuentra gente cerca con la que compartir un plato de comida.', 'Con nuestro buscador encontrarás gente dispuesta a ofrecerte su casa para comer.');?>
+
+    <section style="height:2000px"></section>
+
+     <!-- FOOTER -->
+   <?php get_footer_subfolder();?>
--- index.php
+++ index.php
@@ -95,28 +95,5 @@
         </article>
     </section>
 
-    <!-- FOOTER -->
    <?php get_footer(); ?>
 
-   <!-- MENU LATERAL -->
-   <div id="menu-lateral">
-       <a href="#"><span class="icon-comer"></span></a>
-       <a href="#"><span class="icon-compartir"></span></a>
-       <a href="#"><span class="icon-aprender"></span></a>
-   </div>
-
-   <!-- SCROLL TO TOP -->
-
-   <a href="#" class="go-top"><span class="icon-scroll-top"></span></a>
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-    crossorigin="anonymous"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
